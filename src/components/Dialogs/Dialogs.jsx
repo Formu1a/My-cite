4c8b8e955a47bfae46210bb0fdadeb53dac99874
Formu1a <toz1fq@bk.ru>
2021-09-28 01:11:56 +0300

    New comit

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,10 +2,35 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return(
-        <div >
-        Dialogs
+        <div className={s.Dialogs} >
+            <div className={s.Dialogsitem}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dima
+                </div>
+                {/* <div className={s.dialog}>
+                    <Navlink to='/Arina'>Arina</Navlink>
+                </div>
+                <div className={s.dialog}>
+                    <Navlink to='/Arina'>Anton</Navlink>
+                </div>
+                <div className={s.dialog}>
+                    <Navlink to='/Arina'>Anton</Navlink>
+                </div>
+                <div className={s.dialog}>
+                    <Navlink to='/Arina'>Igor</Navlink>
+                </div>
+                <div className={s.dialog}>
+                    <Navlink to='/Arina'>Yoto</Navlink>
+                </div> */}
+            </div>
+            <div className={s.Massages}>
+                <div className={s.massage}>Hi</div>
+                <div className={s.massage}>How are you</div>
+                <div className={s.massage}>yooooooooo</div>
+
+            </div>
         </div>
     )
     
 }
- export default Dialogs;
+export default Dialogs;
--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import s from './NavBar.module.css'
 
 
@@ -5,20 +6,20 @@ import s from './NavBar.module.css'
 const Navbar =() =>{
 
 return<nav className={s.nav}>
-        <div className={s.item}>
-         <a href="/Profile">Profile</a>
+        <div className={`${s.item} ${s.itemA}`}>
+          <NavLink to="/Profile" activeClassName={s.word}>Profile</NavLink>
         </div>
-        <div className={s.item}>
-         <a href="/Dialogs">Massages</a>
+        <div className={`${s.item} ${s.itemA}`}>
+          <NavLink to="/Dialogs" activeClassName={s.word}>Massages</NavLink>
         </div>
-        <div className={s.item}>
-         <a href='/News'>News</a>
+        <div className={`${s.item} ${s.itemA}`}>
+          <NavLink to='/News' activeClassName={s.word}>News</NavLink>
         </div>
-        <div className={s.item}>
-         <a href='/Music'>Music</a>
+        <div className={`${s.item} ${s.itemA}`}>
+          <NavLink to='/Music' activeClassName={s.word}>Music</NavLink>
         </div>
         <div className={`${s.item} ${s.itemS}`}>
-         <a href='/Settings'>Settings</a>
+          <NavLink to='/Settings' activeClassName={s.word}>Settings</NavLink>
         </div>
       </nav>
 }
